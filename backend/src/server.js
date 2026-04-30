b3eb18a3a04918254a470c2fa0fe068f95f6c725
Veekta <vsunday.1710@gmail.com>
2026-04-30 09:28:20 -0600

    update profile endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
+import cookieParser from 'cookie-parser'
 
 //routes
 import authRoutes from "./routes/auth.route.js"
@@ -17,6 +18,7 @@ const __dirname = path.resolve()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
