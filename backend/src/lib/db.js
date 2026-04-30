8917b64ec97d1d38ed5671fb483dd664c40f43ef
Veekta <vsunday.1710@gmail.com>
2026-04-30 04:41:04 -0600

    signup refactoring- make changes in db error code

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -11,12 +11,14 @@ export const connectDB = async()=>{
         
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log("MONGODB CONNECTED:", conn.connection.host)
-        
+
     } catch (error) {
         // console.log("Error connecting to MONGODB:", error)
 
         //After CR
-         console.error("Error connecting to MONGODB:", error?.message)
-        process.exit(1) // 1 status code means failed, 0 means success
+        //  console.error("Error connecting to MONGODB:", error?.message)
+        // process.exit(1) // 1 status code means failed, 0 means success
+        console.error("Error connecting to MONGODB:", error?.message)
+         throw error
     }
 }
